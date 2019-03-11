b39fcf2b1f6fe502d7adbf68da7f48c1df300c56
NsikakApp <nsikakthompson73@gmail.com>
2019-03-11 13:25:32 +0100

    initial Commit

---
+++ app/src/main/java/ng/com/starthub/constraintlayoutexample/MainActivity.kt
@@ -0,0 +1,12 @@
+package ng.com.starthub.constraintlayoutexample
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
